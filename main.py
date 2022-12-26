e823fc7df2135926f41db96bcb0d939fa6b01642
Linar Galeev <galeev.linar93@gmail.com>
2022-12-26 22:37:52 +0500

    Add annotations

--- main.py
+++ main.py
@@ -14,11 +14,7 @@ def attack(char_name: str, char_class: str) -> str:
     return (f'{char_name} не применил специальное умение')
 
 
-<<<<<<< HEAD
 def defence(char_name: str, char_class: str) -> str:
-=======
-def defence(char_name, char_class):
->>>>>>> 329371d5c755f2b1bec258f89fdf707c09c3fa08
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -28,11 +24,7 @@ def defence(char_name, char_class):
     return (f'{char_name} не применил специальное умение')
 
 
-<<<<<<< HEAD
 def special(char_name: str, char_class: str) -> str:
-=======
-def special(char_name, char_class):
->>>>>>> 329371d5c755f2b1bec258f89fdf707c09c3fa08
     if char_class == 'warrior':
         return (f'{char_name} применил'
                 f'специальное умение «Выносливость {80 + 25}»')
@@ -43,11 +35,7 @@ def special(char_name, char_class):
     return (f'{char_name} не применил специальное умение')
 
 
-<<<<<<< HEAD
 def start_training(char_name: str, char_class: str) -> str:
-=======
-def start_training(char_name, char_class):
->>>>>>> 329371d5c755f2b1bec258f89fdf707c09c3fa08
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
